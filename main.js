a94744a8a8348b267d2c878d0b42b87de7542d7a
Agalya-10 <aagalya360@gmail.com>
2024-10-14 16:48:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+    
+  var rain=false
+  if (rain)
+  {
+      console.log("take an umbrella")
+  }
+  else{
+      console.log("enjoy the sunshine")
+      }
+
+
+      var score = 60;
+
+      if (score < 50) {
+          console.log("You need to improve");
+      } else if (score <= 70) {
+          console.log("Good job");
+      } else if (score > 70) {
+          console.log("Excellent performance");
+      }
+      else{
+        console.log("fail");
+      }
+      
+
+      
+      var a=20;
+      ++a;
+      console.log(a);
+    
+      var a=20;
+      b=a++;
+      console.log(b);
+      
+      var a=60;
+      b=--a;
+      console.log(b);
